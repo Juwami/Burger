7a85f299404ea92ea2593d490e6101b656500ec1
Juwami <sean.nguyenf@yahoo.com>
2019-07-23 17:58:50 -0400

    skeleton code updateOne and change db to just regular burgers

--- config/orm.js
+++ config/orm.js
@@ -2,21 +2,25 @@ let connection = require("./connection")
 
 let orm = {
     selectAll: function() {
-        let query = "SELECT * FROM burgers_db";
+        let query = "SELECT * FROM burgers";
         connection.query(query, function(err, result) {
             if (err) throw err;
             console.log(result);
         })
     },
     insertOne: function(name) {
-        let query = "INSERT INTO burgers_db (burger_name) VALUES (?)";
+        let query = "INSERT INTO burgers (burger_name) VALUES (?)";
         connection.query(query, [name], function(err, result) {
             if (err) throw err;
             console.log(result);
         })
     },
-    updateOne: function() {
-
+    updateOne: function(name, newname) {
+        let query = "UPDATE burger SET ? = ?";
+        connection.query(query, [name, newname], function(err, result) {
+            if (err) throw err;
+            console.log(result)
+        })
     }
 }
 
