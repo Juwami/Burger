e5a4a0454058e79ff028001ca467988f4ab07539
Juwami <sean.nguyenf@yahoo.com>
2019-07-23 17:42:26 -0400

    finished orm for selectAll and insertOne

--- config/orm.js
+++ config/orm.js
@@ -2,10 +2,18 @@ let connection = require("./connection")
 
 let orm = {
     selectAll: function() {
-
+        let query = "SELECT * FROM burgers_db";
+        connection.query(query, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        })
     },
-    insertOne: function() {
-
+    insertOne: function(name) {
+        let query = "INSERT INTO burgers_db (burger_name) VALUES (?)";
+        connection.query(query, [name], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        })
     },
     updateOne: function() {
 
