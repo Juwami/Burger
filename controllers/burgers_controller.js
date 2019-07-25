7015418bb97c4640a0953a5616326d2a465fb5e9
Juwami <sean.nguyenf@yahoo.com>
2019-07-24 21:19:04 -0400

    worked on pushing orm and models burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,13 @@
+// require express
 let express = require("express");
+// creat variable route for express
+let router = express.Router();
+// require burger from models
 let burger = require("../models/burger");
 
 // create route tables with express
+
+
 
 // export route tables
 module.exports = 
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,23 @@
 let orm = require("../config/orm");
 
 // create code that will call the ORM functions using burger specific input for the ORM
+let burger = {
+    selectAll: function (callback) {
+        orm.selectAll(function(res) {
+            callback(res);
+        });
+    },
+    insertOne: function (name, callback) {
+        orm.insertOne(name, function(res) {
+            callback(res);
+        });
+    },
+    updateOne: function(name, newname, callback) {
+        orm.updateOne(name, newname, function(res) {
+            callback(res);
+        });
+    }
+}
 
-module.exports = 
+
+module.exports = burger;
