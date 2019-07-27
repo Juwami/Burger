abb69471232e83a5acd1b63c90b39bb8e6d16372
Juwami <sean.nguyenf@yahoo.com>
2019-07-26 21:17:40 -0400

    set up handlebars

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,25 @@
 let connection = require("./connection")
 
 let orm = {
-    selectAll: function() {
+    selectAll: function(cb) {
         let query = "SELECT * FROM burgers";
         connection.query(query, function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         })
     },
-    insertOne: function(name) {
+    insertOne: function(name, cb) {
         let query = "INSERT INTO burgers (burger_name) VALUES (?)";
         connection.query(query, [name], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         })
     },
-    updateOne: function(name, id) {
+    updateOne: function(name, id, cb) {
         let query = "UPDATE burger SET burger_name = ? WHERE id = ?";
         connection.query(query, [name, id], function(err, result) {
             if (err) throw err;
-            console.log(result)
+            cb(result)
         })
     }
 }
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,10 @@ let burger = require("../models/burger");
 
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
-
+        let hbsObject = {
+            burgers: data
+        }
+        res.render("index", hbsObject)
     })
 })
 
